11238d2d7e0611223858420540f71bb2056d75e9
soma <ligma@mailbox.org>
2025-09-19 20:13:43 +0200

    Added PDF/EPUB handling to kitty's open actions

--- configuration.nix
+++ configuration.nix
@@ -338,15 +338,16 @@
             alsa.support32Bit = true;
             pulse.enable = true;
         };
-        # printing = {
-        #   enable = true;
-        #   drivers = [ pkgs.gutenprintBin pkgs.hplip ];
-        # };
-        # avahi = {
-        #   enable = true;
-        #   nssmdns4 = true;
-        #   openFirewall = true;
-        # };
+        #printing = {
+        #enable = true;
+        #drivers = [pkgs.gutenprintBin pkgs.hplip pkgs.gutenprint pkgs.gutenprintBin pkgs.hplip pkgs.hplipWithPlugin pkgs.postscript-lexmark pkgs.samsung-unified-linux-driver pkgs.splix pkgs.brlaser pkgs.brgenml1lpr pkgs.brgenml1cupswrapper pkgs.cnijfilter2 pkgs.epson-escpr2 pkgs.epson-escpr];
+        #drivers = [pkgs.gutenprintBin pkgs.hplip pkgs.gutenprint pkgs.epson-escpr2 pkgs.epson-escpr pkgs.epsonscan2 pkgs.epson_201310w pkgs.epson_201207w pkgs.epson-201401w pkgs.epson-alc1100 pkgs.epson-workforce-635-nx625-series pkgs.epson-inkjet-printer-workforce-840-series];
+        #};
+        #avahi = {
+        #enable = true;
+        #nssmdns4 = true;
+        #openFirewall = true;
+        #};
         resolved = {
             enable = true;
             llmnr = "false";
@@ -973,10 +974,15 @@
                         mime inode/directory
                         action launch --cwd=$FILE_PATH --type=overlay
 
+                        #zathura swallow only from kitty
+                        protocol file
+                        mime application/pdf application/epub
+                        action launch --type=overlay swallow zathura
+
                         #xdg-open
                         protocol file
                         *
-                        action launch --type=overlay xdg-open $\{FILE_PATH}
+                        action launch --type=overlay xdg-open
                     '';
                 };
             };
