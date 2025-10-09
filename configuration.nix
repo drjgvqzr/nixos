78d773d9d26332c3dfc4aa3988733fd379f4e20b
soma <ligma@mailbox.org>
2025-10-09 22:21:12 +0200

    The keymap configuration for programs.yazi was removed from the NixOS configuration

--- configuration.nix
+++ configuration.nix
@@ -443,68 +443,6 @@
         programs.yazi = {
             enable = true;
             enableZshIntegration = true;
-            keymap = {
-                cmp.prepend_keymap = [
-                    {
-                        on = ["<A-e>"];
-                        run = "arrow prev";
-                    }
-                    {
-                        on = ["<A-n>"];
-                        run = "arrow next";
-                    }
-                ];
-                mgr.prepend_keymap = [
-                    {
-                        on = ["m"];
-                        run = "leave";
-                    }
-                    {
-                        on = ["n"];
-                        run = "arrow 1";
-                    }
-                    {
-                        on = ["e"];
-                        run = "arrow -1";
-                    }
-                    {
-                        on = ["i"];
-                        run = "enter";
-                    }
-                    {
-                        on = ["o"];
-                        run = "open";
-                    }
-                    {
-                        on = ["O"];
-                        run = "open --interactive";
-                    }
-                    {
-                        on = ["N"];
-                        run = "find_arrow";
-                    }
-                    {
-                        on = ["E"];
-                        run = "find_arrow --previous";
-                    }
-                    {
-                        on = ["C-["];
-                        run = "escape";
-                    }
-                    {
-                        on = ["q"];
-                        run = "quit";
-                    }
-                    {
-                        on = ["C-c"];
-                        run = "close";
-                    }
-                    {
-                        on = ["C-z"];
-                        run = "suspend";
-                    }
-                ];
-            };
         };
         programs.radio-cli = {
             enable = true;
