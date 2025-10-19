f624e43decda67b488131862a19988c25b253d6c
soma <ligma@mailbox.org>
2025-10-19 11:54:26 +0200

    The `fontname` shell alias string delimiter changed from double quotes to single quotes

--- zsh.nix
+++ zsh.nix
@@ -420,7 +420,7 @@
             rg = "rg --hyperlink-format=kitty";
             fd = "fd --hyperlink";
             trash = "gtrash restore";
-            fontname = "/run/current-system/sw/bin/ls /nix/var/nix/profiles/system/sw/share/X11/fonts | fzf --ansi | xargs -I {} fc-query /nix/var/nix/profiles/system/sw/share/X11/fonts/{} | grep '^\s\+family:' | cut -d'\"' -f2";
+            fontname = ''/run/current-system/sw/bin/ls /nix/var/nix/profiles/system/sw/share/X11/fonts | fzf | xargs -I {} fc-query /nix/var/nix/profiles/system/sw/share/X11/fonts/{} | grep '^\s\+family:' | cut -d'"' -f2'';
             trashinfo = "gtrash summary";
             newsboat = "newsboat -q -u /home/soma/dx/nixos/misc/newsboat";
             ba = "echo $(cat /sys/class/power_supply/BAT0/capacity)%";
