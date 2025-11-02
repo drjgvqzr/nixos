f42ad6cb32ef1bd9a7101753baf72b1fbbc1620c
soma <ligma@mailbox.org>
2025-11-03 00:29:10 +0100

    Added `any-nix-shell` package and integrated it into fish shell initialization

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,7 @@
         links2
         xdg-utils
         fzf
+        any-nix-shell
         lf
         gtrash
         ghc
@@ -714,20 +715,21 @@
                 traceroute = "grc --colour=on traceroute";
                 uptime = "grc --colour=on uptime";
             };
-            shellInit = ''                set fish_color_command green
-                set fish_greeting
-                set TTY1 (tty)
-                [ "$TTY1" = "/dev/tty1" ] && exec sway
+            shellInit = ''                                set fish_color_command green
+                                set fish_greeting
+                                set TTY1 (tty)
+                                [ "$TTY1" = "/dev/tty1" ] && exec sway
 
-                set -q LS_AFTER_CD || set -xg LS_AFTER_CD true
-                # show directory listing on directory change
-                function __ls_after_cd__on_variable_pwd --on-variable PWD
-                    if test "$LS_AFTER_CD" = true; and status --is-interactive
-                        ls -hpNF --color
-                    end
-                end
+                                set -q LS_AFTER_CD || set -xg LS_AFTER_CD true
+                                # show directory listing on directory change
+                                function __ls_after_cd__on_variable_pwd --on-variable PWD
+                                    if test "$LS_AFTER_CD" = true; and status --is-interactive
+                                        ls -hpNF --color
+                                    end
+                                end
 
-                printf '\e[3 q' '';
+                any-nix-shell fish --info-right | source
+                                printf '\e[3 q' '';
             plugins = [
                 {
                     name = "ctrl-z.fish";
