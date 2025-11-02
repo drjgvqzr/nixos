3d94fea04648962c7a233bf0c5f8388c77ba92a2
soma <ligma@mailbox.org>
2025-11-03 00:25:48 +0100

    Added automatic directory listing on cd via a new fish shell function

--- configuration.nix
+++ configuration.nix
@@ -718,6 +718,15 @@
                 set fish_greeting
                 set TTY1 (tty)
                 [ "$TTY1" = "/dev/tty1" ] && exec sway
+
+                set -q LS_AFTER_CD || set -xg LS_AFTER_CD true
+                # show directory listing on directory change
+                function __ls_after_cd__on_variable_pwd --on-variable PWD
+                    if test "$LS_AFTER_CD" = true; and status --is-interactive
+                        ls -hpNF --color
+                    end
+                end
+
                 printf '\e[3 q' '';
             plugins = [
                 {
