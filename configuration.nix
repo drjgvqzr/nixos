2b5900cc42de7f9546717f6cb9d71250094c0759
soma <ligma@mailbox.org>
2025-11-04 15:52:25 +0100

    Removed a fish shell keybinding for insert mode escape and kept the Enter key binding

--- configuration.nix
+++ configuration.nix
@@ -739,16 +739,6 @@
                 bind -M visual e up-line
                 bind -M visual i forward-char
 
-                bind --preset -M insert ne if commandline -P
-                        commandline -f canceln
-                    else
-                        set fish_bind_mode default
-                        if test (count (commandline --cut-at-cursor | tail -c2)) != 2
-                            commandline -f backward-char
-                        end
-                        commandline -f repaint-mode
-                    end
-
                 bind -M insert en \n\ \ \ \ \ \ \ \ if\ commandline\ -P\n\ \ \ \ \ \ \ \ \ \ \ \ commandline\ -f\ cancel\n\ \ \ \ \ \ \ \ else\n\ \ \ \ \ \ \ \ \ \ \ \ set\ fish_bind_mode\ default\n\ \ \ \ \ \ \ \ \ \ \ \ if\ test\ \(count\ \(commandline\ --cut-at-cursor\ \|\ tail\ -c2\)\)\ !=\ 2\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ commandline\ -f\ backward-char\n\ \ \ \ \ \ \ \ \ \ \ \ end\n\ \ \ \ \ \ \ \ \ \ \ \ commandline\ -f\ repaint-mode\n\ \ \ \ \ \ \ \ end\n\ \ \ \
 
                 set TTY1 (tty)
