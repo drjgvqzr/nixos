383b2be12a5a06c1b5ed04f8f7dd6b6eeff5fcf4
soma <ligma@mailbox.org>
2025-11-06 18:30:21 +0100

    Removed pixel increments from Sway window resize keybindings

--- sway.nix
+++ sway.nix
@@ -108,10 +108,10 @@
                 "mod1+Shift+e" = "move up";
                 "mod1+Shift+i" = "move right";
 
-                "mod1+Ctrl+m" = "resize shrink width 10px";
-                "mod1+Ctrl+n" = "resize shrink height 10px";
-                "mod1+Ctrl+e" = "resize grow height 10px";
-                "mod1+Ctrl+i" = "resize grow width 10px";
+                "mod1+Ctrl+m" = "resize shrink width";
+                "mod1+Ctrl+n" = "resize shrink height";
+                "mod1+Ctrl+e" = "resize grow height";
+                "mod1+Ctrl+i" = "resize grow width";
 
                 "mod1+1" = "workspace number 1";
                 "mod1+2" = "workspace number 2";
