1472eace5117dd34b0ee574987ed88f0bcbc9f04
soma <ligma@mailbox.org>
2025-11-09 11:31:34 +0100

    Added `"video=DSI-1:panel_orientation=right_side_up"` to kernel parameters for the Mini host

--- configuration.nix
+++ configuration.nix
@@ -140,7 +140,7 @@
     boot = {
         kernelParams =
             if config.networking.hostName == "Mini"
-            then ["fbcon=rotate:1"]
+            then ["fbcon=rotate:1" "video=DSI-1:panel_orientation=right_side_up"]
             else [];
         initrd = {
             checkJournalingFS = true;
