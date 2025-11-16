2b1919819f3ce96413b4f6fb66c743411a38cc7b
soma <ligma@mailbox.org>
2025-11-16 14:24:14 +0100

    DNSSEC was removed from systemd-resolved configuration

--- configuration.nix
+++ configuration.nix
@@ -390,7 +390,6 @@
         playerctld.enable = true;
         resolved = {
             enable = true;
-            dnssec = "true";
             domains = ["~."];
             fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
             dnsovertls = "true";
