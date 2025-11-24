fe61700936e46f07956e4511bec88ba9bc1242e7
soma <ligma@mailbox.org>
2025-11-24 15:23:14 +0100

    Changed foot terminal padding from 5x0 to 5x3

--- configuration.nix
+++ configuration.nix
@@ -785,7 +785,7 @@
                 main = {
                     font = "Roboto Mono:size=14";
                     selection-target = "clipboard";
-                    pad = "5x0";
+                    pad = "5x3";
                 };
                 bell.system = false;
                 scrollback.lines = 100000;
