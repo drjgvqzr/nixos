89590208a4dbf473ceda34ba0335b7efaf09f3e3
none <none@none>
2026-09-10 00:35:48 +0200

    Added a blinking underline fish cursor setting to shellInit

--- fish.nix
+++ fish.nix
@@ -282,6 +282,7 @@ in {
 
             set fish_greeting
             fish_default_key_bindings
+            set fish_cursor_default underline blink
 
             [ (tty) = /dev/tty1 ] && exec sway
 
